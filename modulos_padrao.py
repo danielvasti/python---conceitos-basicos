15c41ba1d724f338279dac96d42c543ee8685a27
Daniel Vasti <danielvasticr@gmail.com>
2024-11-19 14:00:39 -0300

    19-11

--- modulos_padrao.py
+++ modulos_padrao.py
@@ -1,1 +1,22 @@
 import math
+
+print(math.pi)
+print(math.log(16, 2))
+
+import datetime
+agora = datetime.datetime.now()
+ano_2000 = datetime.datetime(2000, 1, 1)
+
+print(datetime.datetime.now())
+print(agora - ano_2000)
+
+
+import random
+for _ in range(2):
+    n = random.randint(1, 10)
+    print(n)
+nomes = ['Juliano', 'Marcos', 'Paulo']
+
+for _ in range (2):
+    nome = random.choice(nomes)
+    print(nome)
