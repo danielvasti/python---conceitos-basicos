6ebd5d682ef0a9511e063d133aba1dc20e11de81
Daniel Vasti <danielvasticr@gmail.com>
2024-12-19 18:07:33 -0300

    19/12/2024, voltando aos estudos

---
+++ cifra-cesar.py
@@ -0,0 +1,26 @@
+texto = 'ABCDE'
+chave = 2
+
+minusculas = 'abcdefghijklmnopqrstuvwxyz'
+maiusculas = 'ABDCEFGHIJKLMNOPQRSTUVWXYZ'
+
+cifra = ''
+
+def cifrar_caractere(caractere, seq, chave):
+    indice_atual = seq.index(caractere)
+    novo_indice = indice_atual + chave
+    while novo_indice >= len(seq):
+        novo_indice = novo_indice - len(seq)
+    while novo_indice < 0:
+        novo_indice = novo_indice + len(seq)
+    return seq[novo_indice]
+
+for caractere in texto:
+    if caractere in minusculas:
+        caractere_cifra = cifrar_caractere(caractere, minusculas, chave)
+    elif caractere in maiusculas:
+        caractere_cifra = cifrar_caractere(caractere, maiusculas, chave)
+    else:
+        caractere_cifra = caractere
+    cifra += caractere
+
